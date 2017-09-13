a1111b825e927097dae955875e67b536215e9ed4
Andrew Ellis <ellis@hhd.com.au>
2017-09-13 18:55:15 +1000

    add some dummy phrases. add a 'pick random' method. add default options

--- javascript/ipsum.js
+++ javascript/ipsum.js
@@ -2,9 +2,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Ipsum = (function () {
     function Ipsum() {
+        this.verbs = ['one', 'two', 'three', 'four'];
+        this.nouns = [];
+        this.joiningWords = [];
+        this.actions = [];
+        this.options = {
+            paragraphs: 3,
+            sentenceMin: 3,
+            sentenceMax: 3,
+        };
     }
-    Ipsum.prototype.generate = function () {
-        return 'Lorem ipsum';
+    Ipsum.prototype.pickRandom = function (arr) {
+        return arr[Math.floor(Math.random() * arr.length)];
+    };
+    Ipsum.prototype.generate = function (options) {
+        return "Lorem ipsum " + this.pickRandom(this.verbs);
     };
     return Ipsum;
 }());
