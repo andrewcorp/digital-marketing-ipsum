b82afa17aff4bf5972604627093fbe35a7384b5b
Andrew Ellis <ellis@hhd.com.au>
2017-09-14 09:51:09 +1000

    fix inconsistent indentation

--- typescript/ipsum.ts
+++ typescript/ipsum.ts
@@ -1,37 +1,37 @@
 interface Options {
-  paragraphs: number,
-  sentenceMin: number,
-  sentenceMax: number,
+    paragraphs: number,
+    sentenceMin: number,
+    sentenceMax: number,
 }
 
 export default class Ipsum{
 
-  verbs: Array<string>;
-  nouns: Array<string>;
-  joiningWords: Array<string>;
-  actions: Array<string>;
-  options: Options;
+    verbs: Array<string>;
+    nouns: Array<string>;
+    joiningWords: Array<string>;
+    actions: Array<string>;
+    options: Options;
 
-  constructor(){
-      // Hard code this for now. Move to json later.
-      this.verbs = ['leveraging', 'engaging', 'building', 'targeting'];
-      this.nouns = ['key demographics', 'growth channels', 'social', 'big data'];
-      this.joiningWords = ['in order to', 'so that we', 'with the aim to', 'while remembering to'];
-      this.actions = ['build ROI', 're-target key demographics', 'funnel users', 'blow minds'];
+    constructor(){
+        // Hard code this for now. Move to json later.
+        this.verbs = ['leveraging', 'engaging', 'building', 'targeting'];
+        this.nouns = ['key demographics', 'growth channels', 'social', 'big data'];
+        this.joiningWords = ['in order to', 'so that we', 'with the aim to', 'while remembering to'];
+        this.actions = ['build ROI', 're-target key demographics', 'funnel users', 'blow minds'];
 
-      // Default options
-      this.options = {
-          paragraphs: 3,
-          sentenceMin: 3,
-          sentenceMax: 3,
-      }
-  }
+        // Default options
+        this.options = {
+            paragraphs: 3,
+            sentenceMin: 3,
+            sentenceMax: 3,
+        }
+    }
 
-  pickRandom(arr: Array<string>): string{
-    return arr[Math.floor(Math.random() * arr.length)];
-  }
+    pickRandom(arr: Array<string>): string{
+        return arr[Math.floor(Math.random() * arr.length)];
+    }
 
-  generate(options?: Options): string{
-      return `Lorem ipsum ${this.pickRandom(this.verbs)}`;
-  }
+    generate(options?: Options): string{
+        return `Lorem ipsum ${this.pickRandom(this.verbs)}`;
+    }
 }
