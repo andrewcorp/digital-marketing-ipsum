a8a648feb575352831b45cd8a3afec712ed530f8
Andrew Ellis <ellis@hhd.com.au>
2017-09-14 21:20:45 +1000

    add more words to phrase files

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const ipsum_1 = require("./ipsum");
-const ipsum = new ipsum_1.default({ sentenceMin: 5 });
+const ipsum = new ipsum_1.default({ sentenceMin: 5, sentenceMax: 10, paragraphs: 5 });
 console.log(ipsum.generate());
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,5 +1,5 @@
 import Ipsum from './ipsum';
 
-const ipsum = new Ipsum({sentenceMin: 5});
+const ipsum = new Ipsum({sentenceMin: 5, sentenceMax: 10, paragraphs: 5});
 
 console.log(ipsum.generate());
