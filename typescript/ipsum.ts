920043bba2af0194d3d903f9f6d473ef20cfb01d
Andrew Ellis <ellis@hhd.com.au>
2017-09-19 09:55:58 +1000

    spacing and some more words

--- typescript/ipsum.ts
+++ typescript/ipsum.ts
@@ -13,7 +13,7 @@ interface Opts {
     sentenceMax?: number,
 }
 
-export default class Ipsum{
+export default class Ipsum {
 
     private helpers: Helpers;
     private verbs: any;
@@ -22,7 +22,7 @@ export default class Ipsum{
     private actions: any;
     private opts: Opts;
 
-    constructor(opts: Opts){
+    constructor(opts: Opts) {
         this.helpers = new Helpers();
 
         this.verbs = verbs;
@@ -36,7 +36,7 @@ export default class Ipsum{
         this.opts = {...defaultOpts, ...opts};
     }
 
-    private generateSentence(): string{
+    private generateSentence(): string {
         let verb = this.helpers.titleCase(this.helpers.randomFromArray(this.verbs)),
             noun = this.helpers.randomFromArray(this.nouns),
             join = this.helpers.randomFromArray(this.joins),
@@ -45,12 +45,12 @@ export default class Ipsum{
         return `${verb} ${noun} ${join} ${action}`;
     }
 
-    private generateParagraph(): string{
+    private generateParagraph(): string {
         let sentences = this.helpers.randomIntInRange(this.opts.sentenceMin, this.opts.sentenceMax);
         return this.helpers.range(sentences).map(i => this.generateSentence()).join('. ');
     }
 
-    public generate(): string{
+    public generate(): string {
         return this.helpers.range(this.opts.paragraphs).map(i => this.generateParagraph()).join('.\n\n');
     }
 }
